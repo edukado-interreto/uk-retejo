fda0a6687e20ad2097379c8d398e8bc8e683fff0
Baptiste Darthenay <baptiste.darthenay@gmail.com>
2025-06-06 17:31:07 +0200

    feat: add Registration page

--- apps/home/models.py
+++ apps/home/models.py
@@ -1,8 +1,15 @@
+from django.db import models
 from wagtail.models import Page
-from wagtail.fields import StreamField
+from wagtail.fields import RichTextField, StreamField
 from wagtail.admin.panels import FieldPanel
 
 from apps.home.blocks import HomeStreamBlock
+
+
+class BasicPage(Page):
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [FieldPanel("body")]
 
 
 class HomePage(Page):
@@ -13,8 +20,11 @@ class HomePage(Page):
         help_text="Use this section to list UK pros.",
     )
 
-    content_panels = Page.content_panels + [
-        FieldPanel("body"),
-    ]
+    content_panels = Page.content_panels + [FieldPanel("body")]
 
-    content_panels = Page.content_panels + ["body"]
+
+class RegistrationPage(Page):
+    body = RichTextField(blank=True)
+    raw_html = models.TextField(blank=True, default="")
+
+    content_panels = Page.content_panels + [FieldPanel("raw_html")]
