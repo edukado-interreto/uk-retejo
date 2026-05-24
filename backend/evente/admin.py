fb90d58c9b71b81aaa42c801404150d10612bb76
Baptiste Darthenay <baptiste.darthenay@gmail.com>
2026-05-24 19:06:00 +0200

    chore: remove non floss lib

--- backend/evente/admin.py
+++
@@ -1,7 +0,0 @@
-from .models import EventeFooter
-from django.contrib import admin
-
-
-@admin.register(EventeFooter)
-class EventeFooterAdmin(admin.ModelAdmin):
-    pass
--- backend/evente/blocks/__init__.py
+++
@@ -1,5 +0,0 @@
-from .components import *  # noqa
-from .layouts import *  # noqa
-from .footers import *  # noqa
-from .headers import *  # noqa
-from .widgets import *  # noqa
